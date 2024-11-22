c21ab49029bff9d70db5848dd38c8e427eedb1b2
ANZAR <anzarkhan790@gmail.com>
2024-11-22 16:09:39 +0530

    cloud mysql instance(TiDB Cloud Server)

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 require("dotenv").config(); 
+const fs = require("fs");
 
 const dbConfig = {
   host: process.env.MYSQL_HOST_NAME,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.PORT,
+  ssl: {
+    ca: Buffer.from(process.env.SSL_CERT, "base64").toString("utf-8"), // Use escaped backslashes
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
